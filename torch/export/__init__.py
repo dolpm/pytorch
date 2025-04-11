49af2ced1c92d23af604199c74d2a893993bba6f
angelayi <yiangela7@gmail.com>
2025-04-11 16:55:59 -0700

    [export] Add draft-export to error msg
    ghstack-source-id: ded4cdf2f8b10883f08dbc12ef7710226a325220
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/151065

--- torch/export/__init__.py
+++ torch/export/__init__.py
@@ -285,15 +285,42 @@ def export(
             pre_dispatch=True,
         )
 
-    return _export(
-        mod,
-        args,
-        kwargs,
-        dynamic_shapes,
-        strict=strict,
-        preserve_module_call_signature=preserve_module_call_signature,
-        pre_dispatch=True,
-    )
+    try:
+        return _export(
+            mod,
+            args,
+            kwargs,
+            dynamic_shapes,
+            strict=strict,
+            preserve_module_call_signature=preserve_module_call_signature,
+            pre_dispatch=True,
+        )
+    except Exception as e:
+        draft_export_msg = (
+            "The error above occurred when calling torch.export.export. If you would "
+            "like to view some more information about this error, and get a list "
+            "of all other errors that may occur in your export call, you can "
+            "rerun your program with the `DRAFT_EXPORT=1` envvar, or replace your "
+            "`export()` call with `draft_export()`."
+        )
+
+        # For errors that we know can be caught by draft-export, add the message
+        # to ask users to try out draft-export
+        if isinstance(
+            e,
+            (
+                torch.fx.experimental.symbolic_shapes.GuardOnDataDependentSymNode,
+                torch._subclasses.fake_tensor.UnsupportedOperatorException,
+                torch._dynamo.exc.UserError,
+                torch.fx.experimental.symbolic_shapes.ConstraintViolationError,
+            ),
+        ):
+            new_msg = str(e) + "\n\n" + draft_export_msg
+            e.args = (new_msg,)
+        elif isinstance(e, RuntimeError) and "no fake impl registered" in str(e):
+            new_msg = str(e) + "\n\n" + draft_export_msg
+            e.args = (new_msg,)
+        raise e
 
 
 DEFAULT_PICKLE_PROTOCOL = 2
